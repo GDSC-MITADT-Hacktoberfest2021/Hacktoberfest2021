02b7f59af581a2be0c2b81cb06a32349af0842e7
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-10-15 10:23:00 +0530

    Added Flutter Project

---
+++ Flutter/hacktoberfest2021/android/app/src/main/kotlin/com/example/hacktoberfest2021/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hacktoberfest2021
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/hacktoberfest2021/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hacktoberfest2021/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
