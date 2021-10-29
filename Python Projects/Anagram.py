589be807c9da4338b9f60907f8eb94a9d603423f
CodingNinja678 <SuhaneeHingorani@gmail.com>
2021-10-29 10:59:39 +0530

    Create Anagram.py
    An anagram is a word or phrase that's formed by rearranging the letters of another word or phrase.Eg:Race-->Care

---
+++ Python Projects/Anagram.py
@@ -0,0 +1,22 @@
+str1 = input ("Enter the first word :")
+str2 = input("Enter the second word : ")
+
+# convert both the strings into lowercase
+str1 = str1.lower()
+str2 = str2.lower()
+
+# check if length is same
+if(len(str1) == len(str2)):
+
+    # sort the strings
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
+
+    # checking if sorted char arrays are same
+    if(sorted_str1 == sorted_str2):
+        print(str1 + " and " + str2 + " are anagrams.")
+    else:
+        print(str1 + " and " + str2 + " are not anagrams.")
+
+else:
+    print(str1 + " and " + str2 + " are not anagrams.")
